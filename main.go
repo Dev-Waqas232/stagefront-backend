29e655c9735ce526d3893da8e5c38d6ffd8c2d27
Dev-Waqas232 <devwaqas232@gmail.com>
2026-06-16 12:48:52 +0500

    added basic sql test query

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func main() {
 	defer db.Close(context.Background())
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		var result int
+		err := db.QueryRow(context.Background(), "SELECT 1").Scan(&result)
+
+		if err != nil {
+			http.Error(w, "DB connection failed", http.StatusServiceUnavailable)
+			return
+		}
+
 		fmt.Fprint(w, "Server is running!")
 	})
 
